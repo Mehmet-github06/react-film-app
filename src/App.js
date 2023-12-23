82be5d9e97b853720c83df835ee7bcbd9795ad1f
mehmet dogan <mehmetseymen91@gmail.com>
2023-12-24 00:03:10 +0300

    dark mode ok

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
+import React from "react";
 import Blog from "./components/Blog";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Matrix from "./components/Matrix";
 import Movis from "./components/Movis";
 import Footer from "./components/Footers";
-import React, { useState } from "react";
+import { DarkModeProvider ,useDarkMode} from "./components/DarkModeProvider"; // Dosyanın gerçek yolunu ekleyin
 import "./index.css";
 
 function App() {
-  const [isDarkMode, setDarkMode] = useState(true);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!isDarkMode);
-  };
-
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
   return (
-     <div className={isDarkMode ? "dark" : ""}>
-      <div className="absolute top-1 left-5 cursor-pointer text-lg">
-        <i
-          className={`fa-solid fa-sun text-white ${isDarkMode ? "" : "hidden"}`}
-          id="sun"
-          onClick={toggleDarkMode}
-        ></i>
-        <i
-          className={`fa-solid fa-moon text-black ${isDarkMode ? "hidden" : ""}`}
-          id="moon"
-          onClick={toggleDarkMode}
-        ></i>
+    <DarkModeProvider >
+      {/* DarkModeProvider'ın içindeki bileşenler DarkModeContext kullanabilir */}
+      <div className="dark">
+        <div className="absolute top-1 left-5 cursor-pointer text-lg">
+          {/* Buradaki kullanım useDarkMode hook'u ile context'ten değerleri almak içindir */}
+          <i
+            className={`fa-solid fa-sun text-white ${
+              isDarkMode ? "" : "hidden"
+            }`}
+            id="sun"
+            onClick={toggleDarkMode}
+          ></i>
+          <i
+            className={`fa-solid fa-moon text-black ${
+              isDarkMode ? "hidden" : ""
+            }`}
+            id="moon"
+            onClick={toggleDarkMode}
+          ></i>
+        </div>
+
+        <Header />
+        <Hero />
+        <Movis />
+        <Matrix />
+        <Blog />
+        <Footer />
       </div>
-    <Header/>
-    <Hero/>
-    <Movis/>
-    <Matrix/>
-    <Blog/> 
-    <Footer/>
-      
-    </div>
+    </DarkModeProvider>
   );
 }
 
 export default App;
- 
